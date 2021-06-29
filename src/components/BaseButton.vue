9135fb46c376664d205609bcb7978166ab7a1645
Nunny <nunnyreyes@gmail.com>
2021-06-29 17:31:30 -0400

    app is running

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,26 +1,43 @@
-
 <template>
-<div>
-    <button class="button"> 
-        <h1>Yes</h1>
-    </button>
+    <div>
+            <button class="button"> 
+                <h1>Yes</h1>
+            </button>
 
-    <button class="button"> 
-        <h1>No</h1>
-    </button>
-</div>
+            <button class="button"> 
+                <h1>No</h1>
+            </button>
+    </div>
+
+
 </template>
 
+<script>
 
-
-<script setup>
 export default {
     props: {
         text: {
             type: String,
-            default: 'Click Here'
+            required: true
+        },
+    },
+    
+    data() {
+        return {
+            gym: 'Harlem gym',
         }
+    },
+
+    methods: {
+        addToLog() {
+            
+        }//create method here
+        
     }
 }
 
 </script>
+
+
+
+
