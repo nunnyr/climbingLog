c76041d6804009f8a3a506207a780fd9c987f5e0
Nunny <nunnyreyes@gmail.com>
2021-06-30 18:27:32 -0400

    more notes

--- src/Notes.vue
+++ src/Notes.vue
@@ -23,3 +23,38 @@
 <!-- Any slot without a name will be given the name="default. You are welcomed to omit this as well -->
 
 <!-- Slots are great because they provide data and allow us to see what shows up inside of the child. -->
+
+
+<!--Library.vue-->
+2) then we open up our v-slot in template to bind title with slotProps
+<template>
+    <Book>
+        <template v-slot:title="slotProps">
+            <h1>{{ bookTitle }}</h1>
+        </template>
+    </Book>
+</template>
+
+
+<!--Book.vue-->
+1) we are adding an attribute of bookTitle to our slot
+<template>
+    <div class="book">
+        <slot name="title"
+        
+        
+        
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            bookTitle: 'Child Providing Data'
+        }
+    }
+}
+
+</script>
